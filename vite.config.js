6a19372220db0f46583f419ea1fab2e954feab6f
Muyi663 <140513305+Muyi663@users.noreply.github.com>
2026-02-06 17:55:15 +0800

    feat: 创建 React 应用入口文件 `main.jsx` 并配置 `BrowserRouter` 路由。

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 确保在 GitHub Pages 的子目录下能正确加载资源
+  base: process.env.NODE_ENV === 'production' ? '/01.1.pixel_game/' : '/', // 确保在 GitHub Pages 的子目录下能正确加载资源
 })
